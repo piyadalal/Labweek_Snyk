2a4c5d711e788591b7e103feb2a61b56ffd33485
PRDA5207 <priya.dalal@sky.de>
2026-03-09 17:56:33 +0100

    removed security key added by mistake

--- tenplates/azure_gpt.py
+++ tenplates/azure_gpt.py
@@ -2,7 +2,6 @@ from openai import OpenAI
 
 endpoint = "https://rg-labweek-snyk-4610.openai.azure.com/openai/v1"
 deployment_name = "gpt-4o-mini"
-api_key = "9ZrXloEyj1RxD3FVQ9CBkgDZnUuOItmiyp0TrBoc5ucrTH6DSINDJQQJ99CCACmepeSXJ3w3AAABACOGp9Nt"
 
 client = OpenAI(
     base_url=endpoint,
